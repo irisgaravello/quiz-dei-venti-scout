595618acc286adcaac711996b79fa93f6f336bbf
irisgaravello <famiglia.garavello@gmail.com>
2025-11-07 12:31:30 +0100

    Add files via upload

---
+++ quiz-vento-final-ita/src/App.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import QuizVenti from './QuizVenti'
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <QuizVenti />
+    </div>
+  )
+}
